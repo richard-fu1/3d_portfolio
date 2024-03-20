86133dd2b6d82f1a17130755c5a69c4d74080e71
ditto000 <richard.0125@hotmail.com>
2024-03-20 00:16:04 -0400

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import { Home, About, Contact, Projects } from './pages'
 
 const App = () => {
+  // Test (remove later)
   return (
     <main className='bg-slate-300/20 h-full'>
       <Router>
